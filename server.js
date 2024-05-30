38bb90fcd161e9f53c4aebcd1caa311a1719caf2
fernando-castelo <nando.castelobranco24@hotmail.com>
2024-05-30 14:07:07 -0300

    chore(): adding mongo db connection

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
-const moongose = require('mongoose');
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({ path : './config.env'});
 
+const DB = process.env.DATABASE.replace(
+    '<PASSWORD>',
+    process.env.DATABASE_PASSWORD,
+  );
+  
+mongoose.connect(DB).then((con) => {
+console.log('DB connection sucessful!');
+});
+  
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Aoo running on port ${port}...`);
